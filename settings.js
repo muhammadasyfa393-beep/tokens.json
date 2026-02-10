584c63852b4f496f7dc68d45ad5331c6c4c40336
muhammadasyfa393-beep <muhammad.asyfa393@smp.belajar.id>
2026-02-10 11:22:59 +0700

    settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight Bot',
   author: '‎',
-  botName: "vikky bot",
+  botName: "bawah saa",
   botOwner: 'Professor', // Your name
-  ownerNumber: '-', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '6281181187073', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
